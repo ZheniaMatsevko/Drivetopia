e131c0e876458023eb20da04968cea17a493eb65
Yevheniia Matsevko <yevhenia.matsevko@ukma.edu.ua>
2023-06-02 23:47:11 +0300

    added main menu

---
+++ core/src/com/wheelie/co/Graphics/GraphicConstants.java
@@ -0,0 +1,12 @@
+package com.wheelie.co.Graphics;
+
+import com.badlogic.gdx.Gdx;
+
+public class GraphicConstants {
+    public static final int screenWidth= Gdx.graphics.getWidth();
+    public static final int screenHeight= Gdx.graphics.getHeight();
+    public static final int centerX= Gdx.graphics.getWidth()/2;
+    public static final int centerY= Gdx.graphics.getHeight()/2;
+    public static final int colWidth= Gdx.graphics.getWidth()/8;
+    public static final int rowHeight= Gdx.graphics.getHeight()/8;
+}
---
+++ core/src/com/wheelie/co/Tools/FontFactory.java
@@ -0,0 +1,50 @@
+package com.wheelie.co.Tools;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+import java.util.Locale;
+
+public class FontFactory {
+    public static final String UKRAINIAN_FONT_NAME = "f3.ttf";
+    public static final String ENGLISH_FONT_NAME = "Imperial_Web.ttf";
+    public static final String UKRAINIAN_CHARACTERS = "АаБбВвГгҐґДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщьЮюЯя"
+            + "1234567890.,:;_¡!¿?\"'+-*/()[]={}";
+    private BitmapFont enFont;
+    private BitmapFont ukrFont;
+
+    private BitmapFont generateFont(String fontName, String characters, int size, Color color) {
+
+        // Configure font parameters
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.characters = characters;
+        parameter.size = size;
+        parameter.borderWidth = 1.5F;
+        parameter.color= color;
+        parameter.borderColor = color;
+        // Generate font
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator( Gdx.files.internal(fontName) );
+
+        BitmapFont font = generator.generateFont(parameter);
+
+        // Dispose resources
+        generator.dispose();
+
+        return font;
+    }
+    public BitmapFont getFont(Locale locale) {
+        if      ("en".equals(locale.getLanguage())) return enFont;
+        else if ("uk".equals(locale.getLanguage())) return ukrFont;
+        else throw new IllegalArgumentException("Not supported language");
+    }
+    public void initialize() {
+        // If fonts are already generated, dispose it!
+        if (enFont != null) enFont.dispose();
+        if (ukrFont != null) ukrFont.dispose();
+
+        enFont = generateFont(ENGLISH_FONT_NAME, FreeTypeFontGenerator.DEFAULT_CHARS,100, Color.BLACK);
+        ukrFont = generateFont(UKRAINIAN_FONT_NAME, UKRAINIAN_CHARACTERS,95, Color.WHITE);
+    }
+}
