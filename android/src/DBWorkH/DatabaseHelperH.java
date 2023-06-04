825417d4506f61a083dd5740176fd7770a560977
cryst321 <108535022+cryst321@users.noreply.github.com>
2023-06-04 17:55:12 +0300

    normal working app

--- android/src/DBWorkH/DatabaseHelperH.java
+++ android/src/DBWorkH/DatabaseHelperH.java
@@ -1,5 +1,6 @@
 package DBWorkH;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -14,13 +15,32 @@ public class DatabaseHelperH extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create your database tables using SQL commands
-        String createTableQuery = "CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY, name TEXT)";
+        // Create the "user" table
+        String createTableQuery = "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, email TEXT, password TEXT, name TEXT)";
         db.execSQL(createTableQuery);
+
+        // Insert initial data
+        insertInitialData(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Handle database upgrades if necessary
+        // For example, you can drop the existing table and create a new one
+        if (oldVersion < 2) {
+            db.execSQL("DROP TABLE IF EXISTS user");
+            onCreate(db);
+        }
+    }
+
+    private void insertInitialData(SQLiteDatabase db) {
+        // Insert initial data into the "user" table
+        ContentValues values = new ContentValues();
+        values.put("id", 1);
+        values.put("email", "example@email.com");
+        values.put("password", "password123");
+        values.put("name", "Andrew");
+
+        db.insert("user", null, values);
     }
 }
