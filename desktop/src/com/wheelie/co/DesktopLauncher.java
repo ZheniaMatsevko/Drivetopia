3f78e6645f9790550934213d8a5ceda78f886f97
cryst321 <108535022+cryst321@users.noreply.github.com>
2023-06-04 18:54:19 +0300

    normal working app

--- desktop/src/com/wheelie/co/DesktopLauncher.java
+++ desktop/src/com/wheelie/co/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Drivetopia");
-		new Lwjgl3Application(new Drivetopia(), config);
+		//new Lwjgl3Application(new Drivetopia(), config);
 
 
 	}
