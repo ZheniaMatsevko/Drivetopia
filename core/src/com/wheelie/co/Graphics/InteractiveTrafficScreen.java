f89bbc4dd05420a22e131cffc1d3384b44f2d1cf
M.K <maximus.khomenko@gmail.com>
2023-06-05 15:29:41 +0300

    grass :) (Change background colour)

--- core/src/com/wheelie/co/Graphics/InteractiveTrafficScreen.java
+++ core/src/com/wheelie/co/Graphics/InteractiveTrafficScreen.java
@@ -162,7 +162,7 @@ public class InteractiveTrafficScreen extends ScreenAdapter implements InputProc
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(86/255f, 168/255f, 50/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         sprite.draw(batch);
