79bc548eef43e70d3984fbf4c991d70a0a5689f9
cryst321 <108535022+cryst321@users.noreply.github.com>
2023-06-06 04:25:42 +0300

    add neccessar nesseca nescser i forgot the word classes

---
+++ core/src/com/wheelie/co/levels20/level3.java
@@ -0,0 +1,22 @@
+package com.wheelie.co.levels20;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+import java.util.LinkedList;
+
+public class level3 implements Level{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public LinkedList<ScreenAdapter> getTasks() {
+        return null;
+    }
+
+    @Override
+    public void finish() {
+
+    }
+}
