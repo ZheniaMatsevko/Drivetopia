b9553c623c144a4bba262bbb78a9f07cad7c4266
Yevheniia Matsevko <yevhenia.matsevko@ukma.edu.ua>
2023-06-11 17:41:22 +0300

    add flashCardScreen

--- android/src/com/wheelie/co/levelTemplates/NormalFlashCardQuestionScreen.java
+++ android/src/com/wheelie/co/levelTemplates/NormalFlashCardQuestionScreen.java
@@ -1,9 +1,211 @@
 package com.wheelie.co.levelTemplates;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.wheelie.co.Drivetopia;
+import com.wheelie.co.Graphics.GraphicConstants;
+import com.wheelie.co.Graphics.LevelsScreen;
+import com.wheelie.co.Tools.FontFactory;
+import com.wheelie.co.Tools.MyDialog;
+import com.wheelie.co.levelTemplates.questionTemplates.HardPictureQuestion;
+import com.wheelie.co.levelTemplates.questionTemplates.NormalFlashCardQuestion;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Locale;
 
 public class NormalFlashCardQuestionScreen extends ScreenAdapter implements InputProcessor {
+    Drivetopia app;
+    private SpriteBatch batch;
+    private Sprite sprite;
+    private Stage stage;
+    private int level;
+
+    private BitmapFont font1;
+    private BitmapFont font2;
+    private BitmapFont font3;
+
+    private Skin skinForDialog;
+
+
+    private Skin skin2;
+    private int score;
+    private Locale enLocale;
+    private Locale ukrLocale;
+    private FontFactory fontFactory;
+    private final GlyphLayout layout;
+    private final GlyphLayout layoutUkr;
+    private TextButton exitButton;
+    private int chosenLevel;
+    private NormalFlashCardQuestion question;
+
+    public NormalFlashCardQuestionScreen(NormalFlashCardQuestion question1,final Drivetopia app, String title, int level, int score, int chosenLevel) {
+
+        question=question1;
+        this.chosenLevel=chosenLevel;
+        fontFactory = new FontFactory();
+        fontFactory.initialize();
+
+        this.level =level;
+        this.app = app;
+        this.score = score;
+
+        stage = new Stage(new ScreenViewport());
+
+
+        batch = new SpriteBatch();
+
+
+
+        enLocale = new Locale("en", "US");
+        ukrLocale = new Locale("uk", "UA");
+        font1=fontFactory.getFont(ukrLocale,1);
+        font2=fontFactory.getFont(enLocale,1);
+        font3=fontFactory.getFont(ukrLocale,2);
+
+        BitmapFont fontDialog=fontFactory.getFont(ukrLocale,4);
+
+        // Setting up the skin
+        skinForDialog = new Skin(new TextureAtlas(Gdx.files.internal("skin-composer-ui.atlas")));
+        skinForDialog.add("font", fontDialog);
+        skinForDialog.load(Gdx.files.internal("skin-composer-ui.json"));
+
+
+        Skin skinForText = new Skin(new TextureAtlas(Gdx.files.internal("skin-composer-ui.atlas")));
+        skinForText.add("font", font3);
+
+        skinForText.load(Gdx.files.internal("skin-composer-ui.json"));
+
+
+        skin2 = new Skin(new TextureAtlas(Gdx.files.internal("skin-composer-ui.atlas")));
+        skin2.add("font", font1);
+
+        skin2.load(Gdx.files.internal("skin-composer-ui.json"));
+
+        String text = question.getQuestion();
+
+
+        LinkedList<ImageButton> buttons = question.getObjects();
+        for(int i=0;i<3;i++){
+            buttons.get(i).addListener(new ClickListener() {
+                public void clicked(InputEvent event, float x, float y) {
+                    System.out.println("Hit");
+                    showDialog("Подумай краще");
+                }
+            });
+        }
+
+        question.getCorrectAnswer().addListener(new ClickListener() {
+            public void clicked(InputEvent event,float x, float y) {
+                System.out.println("Hit");
+                showDialog("Молодець");
+            }
+        });
+
+        Collections.shuffle(buttons);
+
+        Table table = new Table();
+        table.defaults().pad(10,10,150,10);
+        table.setPosition(10, 10); // Replace x and y with the desired coordinates
+        table.setSize(GraphicConstants.screenWidth, GraphicConstants.screenHeight);
+
+
+        table.add(buttons.get(0));
+        table.add(buttons.get(1));
+        table.row();
+        table.add(buttons.get(2));
+        table.add(buttons.get(3));
+        table.row();
+        //table.add(label).padLeft(50f).padTop(20).row();
+
+
+        stage.addActor(table);
+
+
+
+
+        //stage.addActor(label);
+
+
+        exitButton = new TextButton("Рятуйте!",skin2);
+        exitButton.setSize(GraphicConstants.colWidth*5,GraphicConstants.rowHeight*0.7F);
+        exitButton.setPosition(GraphicConstants.centerX- exitButton.getWidth()/2,300- exitButton.getHeight()*1.2F);
+
+        exitButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                app.setScreen(new LevelsScreen(app,1,1));
+            }
+        });
+
+        stage.addActor(exitButton);
+
+
+
+        sprite = new Sprite(new Texture(Gdx.files.internal("white.jpg")));
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.input.setInputProcessor(stage);
+        layout = new GlyphLayout(font2, "Level "+ level);
+        layoutUkr = new GlyphLayout(font3, question.getQuestion());
+
+
+    }
+    private void showDialog(String message) {
+        final MyDialog dialog = new MyDialog("результат", skinForDialog);
+        dialog.setMessage(message);
+        dialog.getButtonTable().add("ок");
+        dialog.setColor(Color.BLACK);
+
+        dialog.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                dialog.hide();
+                return true;
+            }
+        });
+
+        dialog.setVisible(true);
+        dialog.show(stage);
+    }
+
+    /**
+     * Малюємо головне меню
+     */
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+        batch.begin();
+        sprite.draw(batch);
+        //font2.draw(batch, "DRIVETOPIA", Gdx.graphics.getWidth()/6,Gdx.graphics.getHeight()/4*3);
+        font2.draw(batch, layout, GraphicConstants.centerX-layout.width/2,GraphicConstants.rowHeight*7.2F);
+        font3.draw(batch, layoutUkr, GraphicConstants.centerX-layoutUkr.width/2,GraphicConstants.rowHeight*1.9F);
+
+        //fontFactory.getFont(ukrLocale).draw(batch, "Приав", Gdx.graphics.getWidth()/4-20,Gdx.graphics.getHeight()/4*3+200);
+
+
+
+        batch.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
     @Override
     public boolean keyDown(int keycode) {
         return false;
--- android/src/com/wheelie/co/levelTemplates/questionTemplates/NormalFlashCardQuestion.java
+++ android/src/com/wheelie/co/levelTemplates/questionTemplates/NormalFlashCardQuestion.java
@@ -1,4 +1,81 @@
 package com.wheelie.co.levelTemplates.questionTemplates;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.wheelie.co.Graphics.GraphicConstants;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class NormalFlashCardQuestion {
+    private LinkedList<ImageButton> objects;
+    private ImageButton correctAnswer;
+    private String question;
+
+    public NormalFlashCardQuestion(int level){
+        initialize(level);
+    }
+
+    private void initialize(int level) {
+        //read from Database
+        question="Пішохідний перехід";
+        objects = new LinkedList<>();
+        for(int i=1;i<4;i++){
+            Texture myTexture = new Texture(Gdx.files.internal("sign"+i+".png"));
+            ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+            style.imageUp = new TextureRegionDrawable(new TextureRegion(myTexture));
+
+            ImageButton obj1 = new ImageButton(style);
+
+            obj1.setSize(500,500);
+            objects.add(obj1);
+        }
+
+
+
+        Texture myTexture1 = new Texture(Gdx.files.internal("zebra.png"));
+        TextureRegionDrawable t = new TextureRegionDrawable(new TextureRegion(myTexture1));
+
+        ImageButton obj = new ImageButton(t);
+        obj.setSize(500,500);
+        correctAnswer=obj;
+        objects.add(correctAnswer);
+
+    }
+
+    public NormalFlashCardQuestion(LinkedList<ImageButton> objects, ImageButton correctAnswer, String question){
+        this.objects = objects;
+        this.question=question;
+        this.correctAnswer=correctAnswer;
+    }
+
+    public ImageButton getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public LinkedList<ImageButton> getObjects() {
+
+        return objects;
+    }
+
+
+    public void setObjects(LinkedList<ImageButton> objects) {
+        this.objects = objects;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setCorrectAnswer(ImageButton correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
 }
